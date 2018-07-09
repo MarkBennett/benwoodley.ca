d9cbdab5c06f129b2badfc8dc79a169092f02974
Mark Bennett <mark@burmis.ca>
2018-07-09 11:46:30 -0600

    Setup benwoodley to redirect to a Google Form.

---
+++ script.js
@@ -0,0 +1,5 @@
+(() => {
+  console.log("Redirecting to RSVP form!");
+  const RSVP_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSetRqZ0BRKrWQ4wgjIEBPZCg4_Zlhnm2jywU3icvd8FVkpO9g/viewform?usp=sf_link";
+  window.location = RSVP_FORM_URL;
+})()
