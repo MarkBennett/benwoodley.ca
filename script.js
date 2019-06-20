5552c09481e2f51764c34277c4de9353dde7f64e
Mark Bennett <mark@burmis.ca>
2019-06-20 12:15:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
   console.log("Redirecting to RSVP form!");
-  const RSVP_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSetRqZ0BRKrWQ4wgjIEBPZCg4_Zlhnm2jywU3icvd8FVkpO9g/viewform?usp=sf_link";
+  const RSVP_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLScrQ0mLxOpE_UvxN6fyKUMoCl1zfH3KTlkotKM7QmSFqQtlWg/viewform?usp=sf_link";
   window.location = RSVP_FORM_URL;
 })()
